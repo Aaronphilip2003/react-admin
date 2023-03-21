81574c17b0231cb138ccf8335bbb98e63fe0d3e5
Aaronphilip2003 <aaronphilip2003@gmail.com>
2023-03-21 08:52:34 +0530

    added map

---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,15 @@
+import Iframe from 'react-iframe';
+
+const Dashboard=()=>{
+    return <div>
+        <Iframe
+  src="/maps/info.html"
+  width="75%"
+  height="500px"
+  frameBorder="0"
+/>
+
+    </div>
+}
+
+export default Dashboard;
